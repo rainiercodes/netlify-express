366302c4a7a5e56f83547668200342137c72ce14
DevTaylor <97462163+ziplabdev@users.noreply.github.com>
2022-01-25 14:52:36 -0800

    init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const serverless = require('serverless-http')
+
+const app = express()
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+
+    res.json({
+
+        'hello': 'hi'
+    })
+})
+
+app.use('/.netlify/functions/index', router)
+
+module.exports.handler = serverless(app)
